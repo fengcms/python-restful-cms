c01f8c753981b7cf27ed4fc3cc7531075bdc8a9a
fungleo <web@fengcms.com>
2018-08-18 05:34:17 +0800

    添加 post 方法

--- study-be/core/query.py
+++ study-be/core/query.py
@@ -21,6 +21,29 @@ def getDict(obj):
         res[column.name] = getattr(obj, column.name)
     return res
 
+# 传入模型，获得该模型字段列表, 用于 ls 方法
+def getFieldList(Obj):
+    res = []
+    for i in dir(Obj):
+        if i[0] != '_':
+            res.append(i)
+    return res
+
+# 传入模型，获得该模型字段字典并赋空值，排除id和time，用于post方法
+def getFieldDict(Obj):
+    res = {}
+    for i in dir(Obj):
+        if i[0] != '_' and i != 'id' and i != 'time':
+            res[i] = ''
+    return res
+
+# 检查提交数据字段是否在表中包含方法
+def checkField(dat, fields):
+    for i in dat:
+        if not i in fields:
+            return False
+    return True
+
 # 查询列表方法
 def ls(className, request):
     if not hasClass(className):
@@ -36,5 +59,36 @@ def ls(className, request):
             for i in res:
                 arr.append(getDict(i))
         return {'list': arr}
+    except Exception as e:
+        return 503
+
+# 添加新数据方法
+def post(className, request):
+    if not hasClass(className):
+        return 404
+    try:
+        classModel = getattr(model, className)
+
+        # 检查数据是否符合模型字段要求
+        fields = getFieldList(classModel)
+        for Data in request['data']:
+            if not checkField(Data, fields):
+                return 400
+        
+        # 添加数据
+        resIds = []
+        modelDict = getFieldDict(classModel)
+        for Data in request['data']:
+            for i in Data:
+                # POST 时传的 id 和 time 字段会被忽略
+                if i != 'id' and i != 'time':
+                    modelDict[i] = Data[i]
+            newData = classModel(**modelDict)
+            session.add(newData)
+            session.flush()
+            session.commit()
+            resIds.append(newData.id)
+        # 将自增ID返回
+        return {'id': resIds}
     except Exception as e:
         return 503
--- study-be/core/rest.py
+++ study-be/core/rest.py
@@ -16,3 +16,17 @@ def ls (request, name):
         return fail('数据查询失败', 503)
     else:
         return fail('服务器内部错误', 500, 500)
+
+def post (request, name):
+    hmupName = str2Hump(name)
+    res = query.post(hmupName, request)
+    if isinstance(res, dict):
+        return ok(res)
+    elif res == 400:
+        return fail('参数错误', 400)
+    elif res == 404:
+        return fail('数据库中没有' + name + '这个表', 404)
+    elif res == 503:
+        return fail('数据添加失败', 503)
+    else:
+        return fail('服务器内部错误', 500, 500)
