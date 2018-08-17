5a0c9b1a7ec10b0db64bf72d1126624f610aef8f
root <root@centos-linux.shared>
2018-08-17 10:49:36 +0800

    add

--- study-be/api/be.py
+++ study-be/api/be.py
@@ -5,6 +5,7 @@ from sanic.response import json
 from config import PREFIX
 
 from core.tool import ok, fail, checkParam
+from core.session import makeToken, checkToken, clearToken, updataToken
 
 FIX = PREFIX['be']
 # 创建 蓝图
@@ -16,12 +17,36 @@ async def login(request):
     dat = request.json
     manageData = {'username': 'admin', 'password': '123456'}
 
+    res = fail('参数错误', 400)
+
     # 检查入参是否正确
     if not checkParam(['account', 'password'], dat):
-        return fail('参数错误', 400)
+        del res.cookies['session']
+        return res
 
+    # 检查用户名密码是否正确
     if dat['account'] != manageData['username'] \
             or dat['password'] != manageData['password']:
-        return fail('用户名或密码错误', 400)
-    return ok('登录成功')
+        res = fail('用户名或密码错误', 400)
+        del res.cookies['session']
+        return res
 
+    # 正常处理
+    session = makeToken(dat['account'], 'manage')
+    res = ok(session)
+    res.cookies['session'] = session
+    res.cookies['session']['httponly'] = True
+
+    return res
+
+# 登出处理
+@bp.get("logout")
+async def logout(request):
+    token = request.cookies.get('session')
+    res = fail('退出失败', 401, 401)
+    cs = checkToken(token)
+    if cs == 0:
+        clearToken(token)
+        res = ok('退出成功')
+    del res.cookies['session']
+    return res
