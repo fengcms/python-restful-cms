66ce8c63b5bcff3897efcfe92afc91f841588e47
fungleo <web@fengcms.com>
2018-08-19 20:02:29 +0800

    完成树形菜单以及上传文件接口

--- study-be/api/be.py
+++ study-be/api/be.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
+import os
 from sanic import Blueprint
 from sanic.response import json
 from config import PREFIX, ANONYMOUS_API as ANYAPI
 from config import PRIVATE_KEY_PATH 
+from config import UPLOAD_PATH, SUPPORT_TYPE
 
-from core.tool import ok, fail, checkParam
-from core.tool import rsaDecrypt
+from core.tool import ok, fail, checkParam, getMd5, rsaDecrypt
 from core.session import makeToken, checkToken, clearToken, updataToken
 from core.app import listView, itemView
 from core.handle import middleHandle
+from core import rest
 
 
 FIX = PREFIX['be']
@@ -68,3 +70,77 @@ async def logout(request):
         res = ok('退出成功')
     del res.cookies['session']
     return res
+
+# 将菜单栏目以树形结构输出
+@bp.route('tree_channel', methods=['GET'])
+async def tree_channel(request):
+    sourceData = rest.getList({'pagesize': -1, 'sort': '-sort,-id'}, 'channel')
+    if sourceData == 1:
+        return fail('服务器内部错误', 500, 500)
+    if sourceData['total'] < 1:
+        return fail('您当前还没有添加任何栏目')
+
+    sourceList = sourceData['list']
+
+    def makeTree(pid, arr):
+        res = []
+        for i in arr:
+            if i['pid'] == pid:
+                rep = makeTree(i['id'], arr)
+                if len(rep) != 0:
+                    i['children'] = rep
+                res.append(i)
+        return res
+    res = makeTree(0, sourceList)
+
+    return ok(res)
+
+# 上传文件接口特殊处理
+@bp.route('upload', methods=['POST'])
+async def upload(request):
+    # 字节码转16进制字符串
+    def bytes2hex(bytes):
+        hexstr = u""
+        for i in range(10):
+            t = u"%x" % bytes[i]
+            if len(t) % 2:
+                hexstr += u"0"
+            hexstr += t
+        return hexstr.lower()
+
+    # 根据16进制字符串获取文件后缀
+    def getSuffix(hexStr):
+        print(hexStr)
+        for i in SUPPORT_TYPE:
+            if i in hexStr:
+                return SUPPORT_TYPE[i]
+        return 400
+
+    # 判断参数是否正确
+    if not request.files and not request.files.get('file'):
+        return fail('参数错误', 400)
+    image = request.files.get('file').body
+
+    # 判断文件是否支持
+    imageSuffix = getSuffix(bytes2hex(image))
+    if imageSuffix == 400:
+        return fail('不支持的文件类型', 400)
+
+    # 组织图片存储路径
+    md5Name = getMd5(image)
+
+    saveDir = UPLOAD_PATH + md5Name[0:2] + '/'
+    savePath = saveDir + md5Name[2:] + '.' + imageSuffix
+    resPath = '/' + md5Name[0:2] + '/' + md5Name[2:] + '.' + imageSuffix
+
+    # 如果文件夹不存在，就创建文件夹
+    if not os.path.exists(saveDir):
+        os.makedirs(saveDir)
+
+    # 将文件写入到硬盘
+    tempFile = open(savePath, 'wb')
+    tempFile.write(image)
+    tempFile.close()
+
+    # 给客户端返回结果
+    return ok({"path": resPath})
--- study-be/config.py
+++ study-be/config.py
@@ -15,3 +15,12 @@ PUBLIC_KEY_PATH = './key/public.pem'
 PRIVATE_KEY_PATH = './key/private.pem'
 
 PAGESIZE = 10
+
+# 配置上传路径
+UPLOAD_PATH = './upload/'
+# 常见图片格式16进制文件头标志
+SUPPORT_TYPE = {
+    'ffd8ffe':'jpg',
+    '89504e470d0a1a0a0000':'png',
+    '474946383961':'gif',
+}
