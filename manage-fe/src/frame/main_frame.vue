e7aaf88c656a8d80c388fa1c3c32bd87cdc7329f
fungleo <web@fengcms.com>
2018-09-05 18:05:52 +0800

    安装饿了么UI组件，更新主路由视图

--- manage-fe/src/frame/main_frame.vue
+++ manage-fe/src/frame/main_frame.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="manage_main">
-    <header class="manage_header">
-      这里将编写管理后台头部通栏
-    </header>
-    <div class="manage_bar">
-      这里将编写管理后台左侧导航栏
-    </div>
-    <div class="manage_body">
-      <router-view />
-    </div>
-  </div>
+  <el-container class="manage_container">
+    <el-header class="manage_header">这里将编写管理后台头部通栏</el-header>
+    <el-container>
+      <el-aside class="manage_bar">这里将编写管理后台左侧导航栏</el-aside>
+      <el-container class="manage_body">
+        <div class="manage_main">
+          <router-view />
+        </div>
+      </el-container>
+    </el-container>
+  </el-container>
 </template>
