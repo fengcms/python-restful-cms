478c086896e09e2ef4f2f190da9f9e57ae1a0f59
fungleo <web@fengcms.com>
2018-09-07 15:27:07 +0800

    顶部通栏组件完成

---
+++ manage-fe/src/coms/frame/main_frame/top_line.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="manage_header_topline">
+    <h1 class="manage_header_logo">
+      <i class="el-icon-menu"></i> 新闻系统管理后台
+    </h1>
+    <div class="manage_header_link">
+      <span @click="clearCache()">清除缓存</span>
+      <router-link to="/system/change_password">修改密码</router-link>
+      <span @click="logout()">退出登录</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    clearCache () {
+      // 致敬 alert('缓存清除成功！')
+      this.$message({
+        message: '缓存清除成功',
+        type: 'success',
+        duration: 1000
+      })
+    },
+    logout () {
+      // 调用退出接口
+      this.$api.get('logout', null, r => {
+        this.$router.push('/login')
+      }, e => {
+        this.$message.error(e.data)
+      })
+    }
+  }
+}
+</script>
--- manage-fe/src/frame/main_frame.vue
+++ manage-fe/src/frame/main_frame.vue
@@ -1,6 +1,9 @@
 <template>
   <el-container class="manage_container">
-    <el-header class="manage_header">这里将编写管理后台头部通栏</el-header>
+    <el-header class="manage_header">
+      <!-- 使用顶部通栏组件 -->
+      <TopLine></TopLine>
+    </el-header>
     <el-container>
       <el-aside class="manage_bar">这里将编写管理后台左侧导航栏</el-aside>
       <el-container class="manage_body">
@@ -11,3 +14,11 @@
     </el-container>
   </el-container>
 </template>
+<script>
+// 引入顶部通栏组件
+import TopLine from '@/coms/frame/main_frame/top_line.vue'
+export default {
+  // 注册组件
+  components: { TopLine }
+}
+</script>
